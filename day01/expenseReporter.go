588021411aae12042169ed3cfc6129400d597fc1
jonny <jonathon.wright@converge.io>
2020-12-01 13:47:33 +0000

    not quite so many test runs

--- day01/expenseReporter.go
+++ day01/expenseReporter.go
@@ -1,5 +1,6 @@
 package day01
 
+// FindPair find the product of 2 numbers in candiates that sum to target
 func FindPair(candidates []int, target int) int {
 	for i := 0; i<len(candidates); i++ {
 		for j := i+1 ; j<len(candidates); j++ {
@@ -11,12 +12,13 @@ func FindPair(candidates []int, target int) int {
 	return target;
 }
 
+// FindTriple find the product of 3 numbers in candiates that sum to target
 func FindTriple(candidates []int, target int) int {
     for i := 0; i<len(candidates); i++ {
         for j := i+1 ; j<len(candidates); j++ {
             for k := j+1 ; k<len(candidates); k++ {
                 if candidates[i] + candidates[j] + candidates[k] == target {
-                    return candidates[i] * candidates[j] * candidates[k]
+					return candidates[i] * candidates[j] * candidates[k]
                 }
             }
         }
