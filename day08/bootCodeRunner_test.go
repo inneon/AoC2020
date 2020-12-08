fd4dcdeb755cb433ceeb8e50f77008ed1eb1c4b7
jonny <jonathon.wright@converge.io>
2020-12-08 13:49:14 +0000

    day08b

--- day08/bootCodeRunner_test.go
+++ day08/bootCodeRunner_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestBootCode(t *testing.T) {
 	t.Run("example 1a", func(t *testing.T) {
-		got := Run([]string{
+		got, code := Run([]string{
 			"nop +0",
 			"acc +1",
 			"jmp +4",
@@ -15,7 +15,29 @@ func TestBootCode(t *testing.T) {
 			"jmp -4",
 			"acc +6",
 		})
-		want := 5
+		wantAccum := 5
+		wantCode := Loop
+		if got != wantAccum {
+			t.Errorf("got %d want %d", got, wantAccum)
+		}
+		if code != wantCode {
+			t.Errorf("got %d want %d", code, wantCode)
+		}
+	})
+
+	t.Run("example 1b", func(t *testing.T) {
+		got := Fix([]string{
+			"nop +0",
+			"acc +1",
+			"jmp +4",
+			"acc +3",
+			"jmp -3",
+			"acc -99",
+			"acc +1",
+			"jmp -4",
+			"acc +6",
+		})
+		want := 8
 		if got != want {
 			t.Errorf("got %d want %d", got, want)
 		}
